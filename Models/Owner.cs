e72f0694975bcf810c48100f638146080bb0db56
Ethan Sawicki <ethansawicki2797@gmail.com>
2023-04-04 03:49:53 -0500

    Added full CRUD to Owner

--- Models/Owner.cs
+++ Models/Owner.cs
@@ -11,6 +11,7 @@
         public string Address { get; set; }
         public string Phone { get; set; }
 
+        public int NeighborhoodId { get; set; }
         public Neighborhood Neighborhood { get; set; }
 
         public List<Doggo> Doggo { get; set; }
